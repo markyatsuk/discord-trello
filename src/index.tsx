6ffe9977e8158dd4a15aca0eea51b31f0eaf1683
Mark Yatsiuk <markiatsuk@gmail.com>
2023-04-25 10:36:59 +0200

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import App from './Components/App/App';
 import SocketProvider from './context/socket.context';
 import { BrowserRouter } from 'react-router-dom';
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
