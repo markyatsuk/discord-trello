279bc2b2e1b7c8572380785efb20bb51b36bc226
Mark Yatsiuk <markiatsuk@gmail.com>
2023-04-25 10:09:17 +0200

    container

---
+++ src/Components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import s from './Container.module.css';
+
+interface IProps {
+    children: React.ReactNode
+}
+
+const Container = ({ children }: IProps) => (
+  <div className={s.container}>{children}</div>
+);
+
+export default Container;
